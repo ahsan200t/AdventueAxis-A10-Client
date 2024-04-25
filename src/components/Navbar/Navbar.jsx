fa476f85d97fefca53cecb421f9ff0f0219bed8a
ahsan200t <ahsanpekua200@gmail.com>
2024-04-26 00:46:13 +0600

    navbar and footer and project setup donr

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,140 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const {user,logOut}=false
+
+    const navLinks = (
+        <>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-primary border border-primary font-semibold"
+                  : "font-semibold"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+    
+          {user && (
+            <li>
+              <NavLink
+                to="/update profile"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-primary border border-primary font-semibold"
+                    : "font-semibold"
+                }
+              >
+                Update Profile
+              </NavLink>
+            </li>
+          )}
+    
+          <li>
+            <NavLink
+              to="/selling details"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-primary border border-primary font-semibold"
+                  : "font-semibold"
+              }
+            >
+              Selling Details
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/register"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-primary border border-primary font-semibold"
+                  : "font-semibold"
+              }
+            >
+              Register
+            </NavLink>
+          </li>
+        </>
+    );
+    return (
+        <div className="navbar bg-gray-100  mb-10 shadow">
+        <div className="navbar-start">
+          <div className="dropdown dropdown-bottom z-10">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost md:hidden lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <a className="text-2xl md:text-4xl font-semibold font-poppins">
+           AdventureAxis
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+  
+        <div className="navbar-end">
+          {user ? (
+            <div className="dropdown dropdown-bottom flex items-center">
+              <label
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar tooltip tooltip-bottom"
+                data-tip={user.displayName}
+              >
+                <div className="w-10 rounded-full">
+                  <img                  
+                    src={user?.photoURL || "https://i.ibb.co/2ykmyLP/ahsan.jpg"}
+                  />
+                </div>
+              </label>
+              <Link to="/">
+                <button onClick={logOut} className="btn btn-sm btn-secondary">
+                  Logout
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <Link to="/login">
+              <button className="btn btn-sm btn-primary">Login</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
